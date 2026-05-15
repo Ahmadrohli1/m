d6a015eb41d87f15fbbcb1260efa8a9cd2bfcda8
Ahmadrohli1 <ahmad@contoh.com>
2026-05-16 02:46:21 +0700

    Cleanup: Remove failed PWA features and restore UI

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE_NAME = 'musik-rohli-v1';
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['./', 'index.html', 'manifest.json', 'Cantik.jpg'])));
-});
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
